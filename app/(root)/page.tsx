7f1a5f0be6425ed7e3eb1514915b2932a1c6adde
skpixels <makkobir79@gmail.com>
2023-08-07 17:42:16 +0300

    navbar section added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 //app/page.tsx
-import { UserButton } from "@clerk/nextjs";
+// import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <>
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      <h1 className="text-white">Home</h1>
+    </>
   );
 }
---
+++ components/shared/TopBar.tsx
@@ -0,0 +1,44 @@
+import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function TopBar() {
+  return (
+    <nav className="topbar">
+      <Link className="flex items-center gap-4" href="/">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+
+      {/* User operation */}
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+      </div>
+
+      {/* Organization */}
+      <OrganizationSwitcher
+        appearance={{
+          elements: {
+            organizationSwitcherTrigger: "py-2 px-4",
+          },
+        }}
+      />
+    </nav>
+  );
+}
+
+export default TopBar;
