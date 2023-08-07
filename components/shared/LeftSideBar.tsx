87566f2ff71d01256ef6ea4717f74ae3e553b411
skpixels <makkobir79@gmail.com>
2023-08-07 18:07:09 +0300

    leftSideBar icons and text added

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 function LeftSideBar() {
-  return <div>LeftSideBar</div>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => (
+          <Link key={link.label} href={link.route} className="leftsidebar_link">
+            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+
+            <p className="text-light-1 max-lg:hidden">{link.label}</p>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default LeftSideBar;
