4e40d3ee90aded6614d0aa20ee8c7bb1140cca2f
skpixels <makkobir79@gmail.com>
2023-08-08 21:38:49 +0300

    activity working fine :)

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,7 +1,51 @@
-function Activity() {
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function Activity() {
+  // Get loggedIn users
+  const user = await currentUser();
+  if (!user) return null;
+
+  // Get user info
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // getActivity
+  const activities = await getActivity(userInfo._id);
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activities.length > 0 ? (
+          <>
+            {activities.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="Profile Picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                      {""}
+                    </span>
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="text-light-2 !text-base-regular">No Activity Here :(</p>
+        )}
+      </section>
     </section>
   );
 }
